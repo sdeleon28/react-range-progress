c9e112cb886605684ddbd3e14669ed1402f6d0e6
Vincent Grafe <vincent.grafe@gmail.com>
2016-08-28 09:30:01 -0400

    forgot to build.

--- dist/index.js
+++ dist/index.js
@@ -10,8 +10,6 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-require('./styles.css');
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Range = function Range(props) {
