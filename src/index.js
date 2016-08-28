401b41a7a4ad10e2de187d84a21c15382642881c
Vincent Grafe <vincent.grafe@gmail.com>
2016-08-28 09:28:05 -0400

    whoops
    gets rid of webpack, sets css inline.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React, { PropTypes } from 'react'
-import './styles.css'
 
 const Range = props => {
   const val = props.value
@@ -20,6 +19,138 @@ const Range = props => {
            style={{
              height: componentHeight + 'px'
            }}>
+           <style dangerouslySetInnerHTML={{
+             __html:
+               `
+               .slider {
+                 position:absolute;
+                 left:0px;
+                 top:0px;
+                 overflow:visible;
+                 z-index:100;
+               }
+
+               .slidershell {
+                 border:0 none;
+                 position:relative;
+                 left:0px;
+                 top:0px;
+                 overflow:visible;
+               }
+
+               .slidertrack {
+                 border:0;
+                 position:absolute;
+                 background:rgba(254, 254, 254, 0.6);
+               }
+
+               .sliderfill {
+                 border:0;
+               	position:absolute;
+                 pointer-events:none;
+               }
+
+               .sliderthumb {
+               	position:absolute;
+                 left:0px;
+                 top:0px;
+                 border:0 none;
+
+               	padding:0px;
+                 margin:0px;
+                 text-align:center;
+
+               	pointer-events:none;
+               }
+
+               input[type=range]::-ms-track {
+                 width:100%;
+                 height:100%;
+
+                 -webkit-appearance:none;
+                 margin:0px;
+                 padding:0px;
+                 border:0 none;
+
+                 background:transparent;
+                 color:transparent;
+                 overflow:visible;
+               }
+
+               input[type=range]::-moz-range-track {
+                 width:100%;
+                 height:100%;
+
+                 -moz-appearance:none;
+                 margin:0px;
+                 padding:0px;
+                 border:0 none;
+
+                 background:transparent;
+                 color:transparent;
+                 overflow:visible;
+               }
+
+               input[type=range] {
+                 cursor: pointer;
+
+                 -webkit-appearance:none;
+                 padding:0px;
+                 border:0 none;
+
+                 background:transparent;
+                 color:transparent;
+                 overflow:visible;
+               }
+
+               input[type=range]:focus::-webkit-slider-runnable-track {
+                 background:transparent;
+                 border:transparent;
+               }
+
+               input[type=range]:focus {
+                 outline: none;
+               }
+
+               input[type=range]::-ms-thumb {
+                 width:12px;
+                 height:12px;
+
+                 border-radius:0px;
+                 border:0 none;
+                 background:transparent;
+               }
+               input[type=range]::-moz-range-thumb {
+                 width:12px;
+                 height:12px;
+
+                 border-radius:0px;
+                 border:0 none;
+                 background:transparent;
+               }
+               input[type=range]::-webkit-slider-thumb {
+                 width:12px;
+                 height:12px;
+
+                 border-radius:0px;
+                 border:0 none;
+                 background:transparent;
+                 -webkit-appearance:none;
+               }
+
+               input[type=range]::-ms-fill-lower {
+                 background:transparent;
+                 border:0 none;
+               }
+               input[type=range]::-ms-fill-upper {
+                 background:transparent;
+                 border:0 none;
+               }
+               input[type=range]::-ms-tooltip {
+                  display: none;
+               }`
+             }}>
+            </style>
            <div
              className='slidertrack'
              style={{
@@ -53,7 +184,9 @@ const Range = props => {
                marginLeft: componentHeight / 2,
                marginRight: componentHeight / 2,
                top: 0,
-               height: componentHeight
+               height: componentHeight,
+               WebkitAppearance: 'none',
+               background: 'transparent',
              }}
              className='slider'
              type='range'
